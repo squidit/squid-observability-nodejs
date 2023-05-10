9dbc4fdc06ddc1b32e9ebdf85af6709ef9ba0519
hmkmartini <hmk.martini@gmail.com>
2023-05-09 23:06:25 -0300

    feat: SquidError is now part of thins bundle for convenience

--- squid_observability.js
+++ squid_observability.js
@@ -2,8 +2,10 @@ const SquidTracer                 = require('squid-tracer');
 const SquidMetrics                = require('squid-metrics');
 const SquidLogger                 = require('squid-logger');
 const SquidObservabilityConfigs   = require('squid-observability-configs');
+const SquidError                  = require('squid-error');
 
 exports.SquidObservabilityConfigs = SquidObservabilityConfigs;
 exports.SquidLogger               = SquidLogger;
 exports.SquidTracer               = SquidTracer;
 exports.SquidMetrics              = SquidMetrics;
+exports.SquidError                = SquidError;
