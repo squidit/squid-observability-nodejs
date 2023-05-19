f06a559531406f46af36fa3eaf952a29fdf59798
hmkmartini <hmk.martini@gmail.com>
2023-05-18 23:33:54 -0300

    fix: SquidError require error fix: SquidHttpError not being exposed by this bundle feat: updated SquidError, SquidLogger and SquidObservabilityConfigs submodules

--- squid_observability.js
+++ squid_observability.js
@@ -1,11 +1,12 @@
-const SquidTracer                 = require('squid-tracer');
-const SquidMetrics                = require('squid-metrics');
-const SquidLogger                 = require('squid-logger');
-const SquidObservabilityConfigs   = require('squid-observability-configs');
-const SquidError                  = require('squid-error');
+const SquidTracer                    = require('squid-tracer');
+const SquidMetrics                   = require('squid-metrics');
+const SquidLogger                    = require('squid-logger');
+const SquidObservabilityConfigs      = require('squid-observability-configs');
+const { SquidError, SquidHttpError } = require('squid-error');
 
 exports.SquidObservabilityConfigs = SquidObservabilityConfigs;
 exports.SquidLogger               = SquidLogger;
 exports.SquidTracer               = SquidTracer;
 exports.SquidMetrics              = SquidMetrics;
 exports.SquidError                = SquidError;
+exports.SquidHttpError            = SquidHttpError;
