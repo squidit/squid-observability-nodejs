58e27e85d7806649a4cb2227e30588a9df6fbe31
hmkmartini <hmk.martini@gmail.com>
2023-05-28 20:16:05 -0300

    feat: added .d.ts file for TypeScript support feat: updated squid-error-nodejs, squid-logger-nodejs, squid-metrics-nodejs, squid-observability-configs-nodejs and squid-tracer-nodejs, which now export .d.ts files as modules

---
+++ squid_observability.d.ts
@@ -0,0 +1,20 @@
+/// <reference path="libraries/squid-tracer-nodejs/squid_tracer.d.ts"/>
+/// <reference path="libraries/squid-metrics-nodejs/squid_metrics.d.ts"/>
+/// <reference path="libraries/squid-logger-nodejs/squid_logger.d.ts"/>
+/// <reference path="libraries/squid-observability-configs-nodejs/squid_observability_configs.d.ts"/>
+/// <reference path="libraries/squid-error-nodejs/squid_error.d.ts"/>
+
+export declare module 'squid-observability' {
+  import * as SquidTracerType from 'squid-tracer';
+  import * as SquidMetricsType from 'squid-metrics';
+  import * as SquidLoggerType from 'squid-logger';
+  import { SquidObservabilityConfigs as SquidObservabilityConfigsType} from 'squid-observability-configs';
+  import { SquidError as SquidErrorType, SquidHttpError as SquidHttpErrorType } from 'squid-error';
+
+  const SquidTracer: typeof SquidTracerType;
+  const SquidMetrics: typeof SquidMetricsType;
+  const SquidLogger: typeof SquidLoggerType;
+  const SquidObservabilityConfigs: typeof SquidObservabilityConfigsType;
+  const SquidError: typeof SquidErrorType;
+  const SquidHttpError: typeof SquidHttpErrorType;
+}
